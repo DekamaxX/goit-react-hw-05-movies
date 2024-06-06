b38a28466574dd1ab81b77cad705c10f78a8b6c3
DekamaxX <dobosz.mateusz1994@gmail.com>
2024-06-06 14:51:58 +0200

    next change App

---
+++ src/utilities/Api.js
@@ -0,0 +1,1 @@
+export const API_KEY = '3a20228651740e727cceb64ee336276a';
---
+++ src/utilities/TrendingMoviesFetch.js
@@ -0,0 +1,15 @@
+
+import axios from 'axios';
+import { API_KEY } from './Api';
+
+export const fetchTrendingMovies = async () => {
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/trending/movie/day?language=en-US&api_key=${API_KEY}`
+    );
+    return response;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+};
